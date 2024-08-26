6ebfaf21f11c174a97986e15deb498db5676ec02
Millie-blip <milcahchep59@gmail.com>
2024-08-26 12:16:32 +0300

    Compound Data Structures

---
+++ Compound_DS.py
@@ -0,0 +1,16 @@
+#Compound data structures are data that can store a large number of values or data types.
+#Examples
+
+
+#List - an array of objects. Objects can be an integer, a string or a float
+my_list = [2, 3.14, [2, 4, 6], "orange"]
+
+#Dictionary - collection of key-value pairs
+my_dict = {"name":"Millie", "surname":"Ngenoh", "age":18}
+
+
+#Tuple - similar to lists but are immutable. 
+#the difference with lists in terms of syntax is that tuples are written with round brackets
+my_tuple = ("banana", 7.5, 20)
+
+
